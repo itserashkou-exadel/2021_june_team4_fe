5460ee5810a53b769459abf7c40afdc06e09de51
Inessa Druzhkova <innakocherzhuk@gmail.com>
2021-06-18 16:46:06 +0300

    Yra's changes: in head componend add styles, dropdown for user menu

--- src/app/shared/head/head.component.ts
+++ src/app/shared/head/head.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-head',
   templateUrl: './head.component.html',
-  styleUrls: ['./head.component.scss']
+  styleUrls: ['./head.component.scss'],
 })
 export class HeadComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('init');
+  }
+
+  discountSearch = new FormControl('');
+  profileMenu = new FormControl('');
+  // MenU = document.getElementById('mat-menu');
+  profileMenuItems = [
+    'Select cathegory',
+    'History',
+    'Favorit',
+    'Active discounts',
+    'Logout',
+    'Close',
+  ];
+  tabItems = ['Home', 'Profile', 'Vendor', 'Statistics'];
+
+  pmClick(ev: Event) {
+    console.log((ev.target as HTMLButtonElement).innerText);
   }
 
 }
