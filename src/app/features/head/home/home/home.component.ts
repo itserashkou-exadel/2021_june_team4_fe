f47583abbeea9126ad12d489b88b17a512e6a41a
Yurii19 <Yurikolechkin@gmail.com>
2021-06-23 13:08:19 +0300

    fix: comment

--- src/app/features/head/home/home/home.component.ts
+++ src/app/features/head/home/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { createSelector } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { IDiscount, IHeadState, IAppState } from '../../../../shared/variables';
+import { IDiscount, IHeadState, IAppState } from 'src/app/shared/variables';
 import { Store } from '@ngrx/store';
-import { state } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
@@ -24,6 +23,8 @@ export class HomeComponent implements OnInit {
     sd.forEach(el => this.inerState = el)
     // console.log(sd)
   }
+
+  // discounts  : []
 
   ngOnInit(): void {
     console.log(this.inerState)
