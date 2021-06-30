23b41e7f33bcd442c261e41f2dc4bcd7be25a81b
sergey <goreslavskiy.sergey@gmail.com>
2021-06-30 10:55:23 +0300

    add second step in stepper

--- src/app/features/head/vendor/vendor/vendor.component.ts
+++ src/app/features/head/vendor/vendor/vendor.component.ts
@@ -1,46 +1,76 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-interface IVendor {
-  name: string;
-  category: string;
-  tag: string;
-}
+const API_URL = 'http://localhost:8080';
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
   styleUrls: ['./vendor.component.scss']
 })
 export class VendorComponent implements OnInit {
-  // === Mock Data ====
-  vendors: IVendor[] = [
-    {name: 'Nike', category: 'Clothes', tag: 'Sport'},
-    {name: 'Domino’s Pizza', category: 'Food', tag: 'Pizza'},
-    {name: 'Nike', category: 'Clothes', tag: 'Sport'},
-    {name: 'Domino’s Pizza', category: 'Food', tag: 'Pizza'},
-  ];
-  // === /Mock Data ====
   vendorForm!: FormGroup;
+  categories!: Array<Object>;
+  tags!: Array<Object>;
+  vendors!: Array<Object>;
 
-  constructor() { }
+  bpForm!: FormGroup;
+
+  constructor( private http: HttpClient ) { }
 
   ngOnInit(): void {
     this.vendorForm = new FormGroup({
-      name: new FormControl(null),
-      category: new FormControl(null),
-      tag: new FormControl(null),
-      description: new FormControl(null),
-      photos: new FormControl(null),
-      locations: new FormControl(null),
-      startDate: new FormControl(null),
-      endDate: new FormControl(null),
-      contacts: new FormControl(null)
-    })
+      name: new FormControl(null, [Validators.required]),
+      category: new FormControl(null, [Validators.required]),
+      tag: new FormControl(null, [Validators.required]),
+      description: new FormControl(null, [Validators.required]),
+      vendorPhotos: new FormControl(null, [Validators.required]),
+      locations: new FormControl(null, [Validators.required]),
+      startDate: new FormControl(null, [Validators.required]),
+      endDate: new FormControl(null, [Validators.required]),
+      contacts: new FormControl(null, [Validators.required])
+    });
+
+    this.bpForm = new FormGroup({
+      bonusProgram: new FormControl(null, [Validators.required]),
+      bpName: new FormControl(null, [Validators.required]),
+      bpPhotos: new FormControl(null, [Validators.required]),
+      bpDescription: new FormControl(null, [Validators.required]),
+      bpStartDate: new FormControl(null, [Validators.required]),
+      bpEndDate: new FormControl(null, [Validators.required]),
+      bpLocations: new FormControl(null, [Validators.required]),
+      bpSize: new FormControl(null, [Validators.required])
+    });
+
+    //Adding default values for dropdown Vendors from server
+    this.http.get<any>(`${API_URL}/vendors`).subscribe(data => {
+      this.vendors = data.map((value: any) => value.name)  
+    });
+
+    //Adding default values for dropdown Categories from server
+    this.http.get<any>(`${API_URL}/categories`).subscribe(data => {
+      this.categories = data.map((value: any) => value.name)
+    });
+
+    //Adding default values for dropdown Tags from server
+    this.http.get<any>(`${API_URL}/tags`).subscribe(data => {
+      this.tags = data.map((value: any) => value.name)
+    });
+
+
   };
 
-  onSubmit() {
-    // console.log(this.vendorForm.value)
+  saveVendor() {
     localStorage.setItem('vendorsFormData', JSON.stringify(this.vendorForm.value))
+  }
+
+  saveBP() {
+    localStorage.setItem('bpFormData', JSON.stringify(this.bpForm.value))
+  }
+
+  // Reset all form's fields after click btn Clear
+  resetForm() {
+    this.vendorForm.reset();
   }
 }
 
