37dfadfb194dcde6d10ef4cb8f098fce01668781
Yurii19 <Yurikolechkin@gmail.com>
2021-07-06 17:10:46 +0300

    chore: added form controls for tags and cities

--- src/app/features/head/home/home/side-bar-filter/side-bar-filter.component.ts
+++ src/app/features/head/home/home/side-bar-filter/side-bar-filter.component.ts
@@ -1,13 +1,43 @@
  import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-side-bar-filter',
   templateUrl: './side-bar-filter.component.html',
   styleUrls: ['./side-bar-filter.component.scss'],
+  
 })
 
-export class SideBarFilterComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+export class SideBarFilterComponent implements OnInit {
+ 
+  filterForm: FormGroup;
+  filterConfig: any;
+
+  constructor() {
+
+    this.filterForm = new FormGroup({
+      cathegory : new FormControl('Fashion'),
+      cities: new FormControl('Kyiv'),
+    })
+
+    this.filterConfig = {
+      cathegories: ['Food', 'Fashion', 'Gadgets'],
+      cities: ['Kyiv','Lviv','Odessa'],
+    }
+    //const cathegories=
+
+  }
+
+  ngOnInit(): void {
+    
+    
+  }
+
+  selectCathegory(){
+    const cathegoryValue = this.filterForm.get('cathegory')?.value;
+
+    console.log(this.filterConfig);
+  }
 }
