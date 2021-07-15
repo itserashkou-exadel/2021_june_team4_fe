cbd892dc01fc5af29bb6477305b9fd996e3ceb0c
Ivan Shevchuk <formula357357@gmail.com>
2021-07-15 16:42:11 +0300

    profile process

--- src/app/features/head/profile/profile/profile.component.ts
+++ src/app/features/head/profile/profile/profile.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
 
   ngOnInit(): void {
   }
+  
+  // inc() {
+  //   this.store.dispatch(inc());
+  // }
+  // dec() {
+  //   this.store.dispatch(dec());
+  // }
+  // res() {
+  //   this.store.dispatch(res());
+  // }
+  
 
+  // Display(info: string) {
+  //   let Subscr = document.querySelector('#Subscr');
+  //   let Favor = document.querySelector('#Favor');
+  //   let Hist = document.querySelector('#Hist');
+  //   let Active = document.querySelector('#Active');
+
+  //   switch (info) {
+  //     case 'Subscr':
+  //       Subscr?.classList.toggle('d-block')
+  //       Favor?.classList.remove('d-block')
+  //       Hist?.classList.remove('d-block')
+  //       Active?.classList.remove('d-block')
+  //       break;
+  //     case 'Favor':
+  //       Subscr?.classList.remove('d-block')
+  //       Favor?.classList.toggle('d-block')
+  //       Hist?.classList.remove('d-block')
+  //       Active?.classList.remove('d-block')
+  //       break;
+  //     case 'Hist':
+  //       Subscr?.classList.remove('d-block')
+  //       Favor?.classList.remove('d-block')
+  //       Hist?.classList.toggle('d-block')
+  //       Active?.classList.remove('d-block')
+  //       break;
+  //     case 'Active':
+  //       Subscr?.classList.remove('d-block')
+  //       Favor?.classList.remove('d-block')
+  //       Hist?.classList.remove('d-block')
+  //       Active?.classList.toggle('d-block')
+  //       break;
+  //   }
+  // }
 }
