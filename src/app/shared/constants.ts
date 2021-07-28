e1da3fbc855d97b28b4310cd5a4ce40e6c0bc733
Yurii19 <Yurikolechkin@gmail.com>
2021-07-29 02:46:02 +0300

    merge with fix-dev

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://discount-exadel.herokuapp.com';
 export const REFRESH_URL = API_URL + '/authenticate/refresh';
 export const LOGIN_URL = API_URL + '/authenticate/login';
 export const DISCOUNT_URL = API_URL + '/discounts/';
