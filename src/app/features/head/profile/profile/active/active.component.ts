0abae729f09832516a4d3aceae0d8f5099296b25
Ivan Shevchuk <formula357357@gmail.com>
2021-07-28 15:48:24 +0300

    some adaptive and profile

--- src/app/features/head/profile/profile/active/active.component.ts
+++ src/app/features/head/profile/profile/active/active.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../../../../core/services/profile.service';
+import { Observable } from 'rxjs';
+import { IFavoritesProfile } from '../../../../../shared/interfaces';
+import { map } from 'rxjs/operators';
+import { HomeService } from '../../../../../core/services/home.service';
 
 @Component({
   selector: 'app-active',
   templateUrl: './active.component.html',
-  styleUrls: ['./active.component.scss']
+  styleUrls: ['./active.component.scss'],
 })
 export class ActiveComponent implements OnInit {
+  profileCoupons$!: Observable<IFavoritesProfile[]>;
 
-  constructor() { }
+  constructor(
+    private profile: ProfileService,
+    public homeService: HomeService
+  ) {}
 
   ngOnInit(): void {
+    this.profileCoupons$ = this.profile.getCoupons().pipe(
+      map((data: any) => {
+        return data;
+      })
+    );
   }
-
 }
--- src/app/features/head/profile/profile/profile.component.ts
+++ src/app/features/head/profile/profile/profile.component.ts
@@ -1,20 +1,7 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {Observable, Subscription} from 'rxjs';
-import { Store } from '@ngrx/store';
-
-import { CategoriesService } from 'src/app/core/services/categories.service';
-import { HttpClient } from '@angular/common/http';
-import { ICategory } from 'src/app/shared/interfaces';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { IUser } from 'src/app/shared/interfaces';
 import { ProfileService } from 'src/app/core/services/profile.service';
-import { IDescription } from '../../../../shared/interfaces';
-import { ActivatedRoute, Router } from "@angular/router";
-
-export interface IProfileSubscription {
-  categoryAddRemove: string;
-  bgColor: string;
-  added: boolean;
-}
 
 @Component({
   selector: 'app-profile',
@@ -22,53 +9,23 @@ export interface IProfileSubscription {
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  // coupons: any = { promo: '213123123', name: 'Vendor Name' };
-  displayCategory: string[] = ['Category', 'Add/Remove'];
-  categoryAddRemove: string[] = ['COMMON.Global.add', 'COMMON.Global.remove'];
-  displayHistory: string[] = ['Name', 'Promo', 'EndDate'];
+  //categoryAddRemove: string[] = ['COMMON.Global.add', 'COMMON.Global.remove'];
+
   user$: Observable<IUser>;
 
-  categories$: Observable<ICategory[]>;
-  // subscribe$: Observable<any>;
-  DescriptionAll$: Observable<IDescription[]>;
-  // profileHistory$: Observable<IFavoritesProfile[]>;
+  tabs: [
+    { tabName: string; path: string; isActive: boolean },
+    { tabName: string; path: string; isActive: boolean },
+    { tabName: string; path: string; isActive: boolean }
+  ] = [
+    { tabName: 'History', path: 'history', isActive: true },
+    { tabName: 'Favorites', path: 'favorite', isActive: false },
+    { tabName: 'Active Discounts', path: 'active', isActive: false },
+  ];
 
-  tabs:[{ tabName: string; path: string ,isActive: boolean}, { tabName: string; path: string,isActive: boolean }, { tabName: string; path: string,isActive: boolean }] = [
-    { tabName: 'history', path: 'history', isActive: true},
-    { tabName: 'favorite', path: 'favorite', isActive: false},
-    { tabName: 'active', path: 'active', isActive: false}
-  ]
-
-
-  constructor(
-    private store: Store<{ profile: boolean }>,
-    private http: HttpClient,
-    private categories: CategoriesService,
-    private profile: ProfileService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private change: ChangeDetectorRef,
-
-  ) {
-    // this.subscribe$ = store.select('profile');
-    this.categories$ = this.categories.getCategories();
+  constructor(private profile: ProfileService) {
     this.user$ = this.profile.getUser();
-    // this.profileHistory$ = this.profile.getFavorite();
-    // this.favorites$.subscribe(data => console.log(data));
-    this.DescriptionAll$ = this.profile.getDescriptionAll();
   }
 
   ngOnInit() {}
-
-  // items(id: any) {
-  //   console.log('Category Id' + id);
-  // }
-
-  // subscr() {
-  //   this.store.dispatch(addSubscribe());
-  // }
-  // unSubscr() {
-  //   this.store.dispatch(removeSubscribe());
-  //   console.log('removed');
-  // }
 }
